0df7dd14ad4f4d28beb8b9a3f148795610ebaac1
nilknarflima <backendfranklin@gmail.com>
2021-12-09 18:37:05 -0400

    SistemaAluno

---
+++ sistema.js
@@ -0,0 +1,33 @@
+let numeroDeAlunos = 10;
+let contador = 0;
+while(contador <=numeroDeAlunos){
+for (let contador = 0; contador < numeroDeAlunos;) {
+    //console.log(contador)
+
+    if(contador == 0){
+        console.log("O numero atual é zero")
+    } else if(contador % 2 == 0){
+        console.log("O numero é par")
+    }else{
+        console.log(`O numero ${contador} é impar`)
+    }
+    
+}
+
+
+
+
+// let numeroDeAlunos = 10;
+
+// for (let contador = 0; contador < numeroDeAlunos; contador++) {
+//     //console.log(contador)
+
+//     if(contador == 0){
+//         console.log("O numero atual é zero")
+//     } else if(contador % 2 == 0){
+//         console.log("O numero é par")
+//     }else{
+//         console.log(`O numero ${contador} é impar`)
+//     }
+    
+//}
